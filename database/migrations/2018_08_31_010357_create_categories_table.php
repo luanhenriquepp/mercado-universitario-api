810527502309d233107394a19bbbaf6150a82375
9208 <lhppaixaoo@gmail.com>
2018-08-30 22:15:56 -0300

    Iniciando o cadastro de anúncios

---
+++ database/migrations/2018_08_31_010357_create_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_category', function (Blueprint $table) {
+            $table->increments('cd_category');
+            $table->string('ds_category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_category');
+    }
+}
---
+++ database/migrations/2018_08_31_010358_create_advertisements_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdvertisementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_advertisement', function (Blueprint $table) {
+            $table->increments('cd_advertisement');
+            $table->string('title');
+            $table->text('ds_advertisement');
+            $table->double('price');
+            $table->integer('cd_user')->unsigned();
+            $table->foreign('cd_user','cd_user_fk')
+                ->references('cd_user')
+                ->on('tb_user');
+            $table->integer('cd_category')->unsigned();
+            $table->foreign('cd_category','cd_category_fk')
+                ->references('cd_Category')
+                ->on('tb_category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_advertisement');
+    }
+}
